96b9b1bbd2d514991110fffdf18b0622ce513e56
Hiroaki Tateshita <tateshita.hiroaki@gmail.com>
2018-01-10 00:16:58 +0900

    one click changes centre node. still on going for put appopriate image to background.

--- src/main/java/org/braincopy/jspwiki/plugin/CytoscapePlugin.java
+++ src/main/java/org/braincopy/jspwiki/plugin/CytoscapePlugin.java
@@ -32,8 +32,13 @@ import org.apache.wiki.WikiContext;
 import org.apache.wiki.WikiEngine;
 import org.apache.wiki.WikiPage;
 import org.apache.wiki.api.exceptions.PluginException;
+import org.apache.wiki.api.exceptions.ProviderException;
 import org.apache.wiki.api.plugin.WikiPlugin;
+import org.apache.wiki.attachment.Attachment;
+import org.apache.wiki.attachment.AttachmentManager;
+import org.braincopy.IllegalFileNameException;
 import org.braincopy.Link;
+import org.braincopy.Picture;
 
 /**
  * @author Hiroaki Tateshita
@@ -75,127 +80,174 @@ public class CytoscapePlugin implements WikiPlugin {
 		TreeSet<Link> edgeSet = new TreeSet<Link>();
 
 		WikiEngine engine = context.getEngine();
-		result += readNodeAndEdge(engine, pagename, nodeSet, edgeSet, depth, depth);
 
-		result += "hello " + pagename + "<br>\n";
-		result += "total page number: " + nodeSet.size() + "<br/>\n";
-		result += "<style>\n";
-		result += "\t#cy {\n";
-		result += "\t\twidth: 95%;\n";
-		result += "\t\theight: 100%;\n";
-		result += "\t\tposition: absolute;\n";
-		result += "\t\ttop: 30px;\n";
-		result += "\t\tright: 30px;\n";
-		result += "\t}\n";
-		result += "</style>\n";
-		result += "<div id='cy'></div>\n";
+		try {
+			result += readNodeAndEdge(engine, pagename, nodeSet, edgeSet, depth, depth);
 
-		result += "<br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> \n";
+			result += "hello " + pagename + "<br>\n";
+			result += "total page number: " + nodeSet.size() + "<br/>\n";
+			result += "<style>\n";
+			result += "\t#cy {\n";
+			result += "\t\twidth: 95%;\n";
+			result += "\t\theight: 100%;\n";
+			result += "\t\tposition: absolute;\n";
+			result += "\t\ttop: 30px;\n";
+			result += "\t\tleft: 100px;\n";
+			result += "\t}\n";
+			result += "</style>\n";
+			result += "<div id='cy'></div>\n";
 
-		// result += "<script src='/personal/scripts/cytoscape.js'></script>\n";
-		// result += "<script src='/personal/scripts/cola.js'></script>\n";
-		// result += "<script src='/personal/scripts/cytoscape-cola.js'></script>\n";
-		result += "<script src='https://braincopy.org/WebContent/js/cytoscape.js'></script>\n";
-		result += "<script src='https://braincopy.org/WebContent/js/cola.js'></script>\n";
-		result += "<script src='https://braincopy.org/WebContent/js/cytoscape-cola.js'></script>\n";
-		result += "<script>\n";
-		result += "\tvar cy = cytoscape({\n";
-		result += "\t\tcontainer: document.getElementById('cy'),\n";
-		result += "\t\telements: [\n";
+			// result += "<br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> \n";
 
-		Iterator<PageInformation> localNodesIte = nodeSet.iterator();
-		Iterator<Link> localLinksIte = edgeSet.iterator();
-		PageInformation nodePageInfo = null;
-		while (localNodesIte.hasNext()) {
-			nodePageInfo = localNodesIte.next();
-			result += "\t\t\t{ data: { id: '" + nodePageInfo.getPageName() + "',\n";
-			result += "\t\t\t\t group: 'L" + nodePageInfo.getStep() + "'} },\n";
+			// result += "<script src='/personal/scripts/cytoscape.js'></script>\n";
+			// result += "<script src='/personal/scripts/cola.js'></script>\n";
+			// result += "<script src='/personal/scripts/cytoscape-cola.js'></script>\n";
+			result += "<script src='https://braincopy.org/WebContent/js/cytoscape.js'></script>\n";
+			result += "<script src='https://braincopy.org/WebContent/js/cola.js'></script>\n";
+			result += "<script src='https://braincopy.org/WebContent/js/cytoscape-cola.js'></script>\n";
+			result += "<script>\n";
+			result += "\tvar cy = cytoscape({\n";
+			result += "\t\tcontainer: document.getElementById('cy'),\n";
+			result += "\t\telements: [\n";
+
+			Iterator<PageInformation> localNodesIte = nodeSet.iterator();
+			Iterator<Link> localLinksIte = edgeSet.iterator();
+			PageInformation nodePageInfo = null;
+			while (localNodesIte.hasNext()) {
+				nodePageInfo = localNodesIte.next();
+				result += "\t\t\t{ data: { id: '" + nodePageInfo.getPageName() + "',\n";
+				result += "\t\t\t\t group: 'L";
+				if (nodePageInfo.getStep() <= 1) {
+					result += nodePageInfo.getStep();
+					if (nodePageInfo.getPicture() != null) {
+						result += "',\n";
+						result += "\t\t\t\t pic: 'attach/" + nodePageInfo.getName() + "/"
+								+ nodePageInfo.getPicture().getFileName() + "'";
+					} else {
+						result += "WOP'";
+					}
+				} else {
+					result += nodePageInfo.getStep() + "'";
+				}
+				result += "} },\n";
+			}
+			Link link = null;
+			while (localLinksIte.hasNext()) {
+				link = localLinksIte.next();
+				result += "\t\t\t{ data: {\n";
+				result += "\t\t\t\tid: '_" + link.getName() + "',\n";
+				result += "\t\t\t\tsource: '" + link.getSource() + "',\n";
+				result += "\t\t\t\ttarget: '" + link.getTarget() + "'}},\n";
+			}
+			result += "\t\t\t{ data: { id: '" + pagename + "' } }\n";
+
+			result += "\t\t],\n";
+
+			result += "\t\tlayout : {\n";
+			result += "\t\t\tname : 'cola',\n";
+			result += "\t\t\tmaxSimulationTime: 600000,\n";
+			result += "\t\t\tpadding: 10},\n";
+
+			result += "\t\tstyle: [{\n";
+			result += "\t\t\tselector: 'node',\n";
+			result += "\t\t\tstyle: {\n";
+			result += "\t\t\t\tshape: 'roundrectangle',\n";
+			result += "\t\t\t\twidth: 20,\n";
+			result += "\t\t\t\theight: 20,\n";
+			result += "\t\t\t\topacity:0.3,\n";
+			result += "\t\t\t\t'background-color': 'blue',\n";
+			result += "\t\t\t}\n";
+			result += "\t\t},\n";
+			result += "\t\t{\n";
+			result += "\t\t\tselector: 'node[group=\"L0\"]',\n";
+			result += "\t\t\tstyle: {\n";
+			result += "\t\t\t\tshape: 'roundrectangle',\n";
+			result += "\t\t\t\t'background-color': 'blue',\n";
+			result += "\t\t\t\t'background-image': 'data(pic)',\n";
+			result += "\t\t\t\t'background-fit': 'contain',\n";
+			result += "\t\t\t\twidth: 100,\n";
+			result += "\t\t\t\theight: 100,\n";
+			result += "\t\t\t\topacity:0.8,\n";
+			result += "\t\t\t\t'font-size': 30,\n";
+			result += "\t\t\t\tlabel: 'data(id)'\n";
+			result += "\t\t\t}\n";
+			result += "\t\t},\n";
+			result += "\t\t{\n";
+			result += "\t\t\tselector: 'node[group=\"L0WOP\"]',\n";
+			result += "\t\t\tstyle: {\n";
+			result += "\t\t\t\tshape: 'roundrectangle',\n";
+			result += "\t\t\t\t'background-color': 'blue',\n";
+			result += "\t\t\t\twidth: 100,\n";
+			result += "\t\t\t\theight: 100,\n";
+			result += "\t\t\t\topacity:0.8,\n";
+			result += "\t\t\t\t'font-size': 30,\n";
+			result += "\t\t\t\tlabel: 'data(id)'\n";
+			result += "\t\t\t}\n";
+			result += "\t\t},\n";
+			result += "\t\t{\n";
+			result += "\t\t\tselector: 'node[group=\"L1\"]',\n";
+			result += "\t\t\tstyle: {\n";
+			result += "\t\t\t\tshape: 'roundrectangle',\n";
+			result += "\t\t\t\t'background-color': 'blue',\n";
+			result += "\t\t\t\t'background-image': 'data(pic)',\n";
+			result += "\t\t\t\t'background-fit': 'contain',\n";
+			result += "\t\t\t\twidth: 60,\n";
+			result += "\t\t\t\theight: 60,\n";
+			result += "\t\t\t\topacity:0.7,\n";
+			result += "\t\t\t\t'font-size': 20,\n";
+			result += "\t\t\t\tlabel: 'data(id)'\n";
+			result += "\t\t\t}\n";
+			result += "\t\t},\n";
+			result += "\t\t{\n";
+			result += "\t\t\tselector: 'node[group=\"L1WOP\"]',\n";
+			result += "\t\t\tstyle: {\n";
+			result += "\t\t\t\tshape: 'roundrectangle',\n";
+			result += "\t\t\t\t'background-color': 'blue',\n";
+			result += "\t\t\t\twidth: 60,\n";
+			result += "\t\t\t\theight: 60,\n";
+			result += "\t\t\t\topacity:0.7,\n";
+			result += "\t\t\t\t'font-size': 20,\n";
+			result += "\t\t\t\tlabel: 'data(id)'\n";
+			result += "\t\t\t}\n";
+			result += "\t\t},\n";
+			result += "\t\t{\n";
+			result += "\t\t\tselector: 'node[group=\"L2\"]',\n";
+			result += "\t\t\tstyle: {\n";
+			result += "\t\t\t\tshape: 'roundrectangle',\n";
+			result += "\t\t\t\t'background-color': 'blue',\n";
+			result += "\t\t\t\twidth: 40,\n";
+			result += "\t\t\t\theight: 40,\n";
+			result += "\t\t\t\topacity:0.6,\n";
+			result += "\t\t\t\t'font-size': 10,\n";
+			result += "\t\t\t\tlabel: 'data(id)'\n";
+			result += "\t\t\t}\n";
+			result += "\t\t},\n";
+			result += "\t\t{\n";
+			result += "\t\t\tselector: 'edge',\n";
+			result += "\t\t\tstyle: {\n";
+			result += "\t\t\t\t'width':1,\n";
+			result += "\t\t\t\t'line-color': '#c0c',\n";
+			result += "\t\t\t\t'target-arrow-shape': 'triangle',\n";
+			result += "\t\t\t\t'target-arrow-color': '#c0c',\n";
+			result += "\t\t\t\t'curve-style': 'segments'\n";
+			result += "\t\t\t}\n";
+			result += "\t\t}]\n";
+			result += "\t});\n";
+			result += "\tcy.on('click', 'node', function(evt) {\n";
+			result += "\t\tvar arg = new Object;\n";
+			result += "\t\tvar pair=location.search.substring(1).split('&');\n";
+			result += "\t\tfor(var i=0;pair[i];i++) {\n";
+			result += "\t\t\tvar kv = pair[i].split('=');\n";
+			result += "\t\t\targ[kv[0]]=kv[1];\n";
+			result += "\t\t}\n";
+			result += "\t\twindow.location.href='Wiki.jsp?page='+arg.page+'&target_node='+this.id();\n";
+			result += "\t});\n";
+			result += "</script>\n";
+
+		} catch (ProviderException e) {
+			result += "something happens related to Attachment Issues.";
+			e.printStackTrace();
 		}
-		Link link = null;
-		while (localLinksIte.hasNext()) {
-			link = localLinksIte.next();
-			result += "\t\t\t{ data: {\n";
-			result += "\t\t\t\tid: '_" + link.getName() + "',\n";
-			result += "\t\t\t\tsource: '" + link.getSource() + "',\n";
-			result += "\t\t\t\ttarget: '" + link.getTarget() + "'}},\n";
-		}
-		result += "\t\t\t{ data: { id: '" + pagename + "' } }\n";
-
-		result += "\t\t],\n";
-
-		result += "\t\tlayout : {\n";
-		result += "\t\t\tname : 'cola',\n";
-		result += "\t\t\tmaxSimulationTime: 600000,\n";
-		result += "\t\t\tpadding: 10},\n";
-
-		result += "\t\tstyle: [{\n";
-		result += "\t\t\tselector: 'node',\n";
-		result += "\t\t\tstyle: {\n";
-		result += "\t\t\t\tshape: 'roundrectangle',\n";
-		result += "\t\t\t\twidth: 20,\n";
-		result += "\t\t\t\theight: 20,\n";
-		result += "\t\t\t\topacity:0.3,\n";
-		result += "\t\t\t\t'background-color': 'red',\n";
-		result += "\t\t\t}\n";
-		result += "\t\t},\n";
-		result += "\t\t{\n";
-		result += "\t\t\tselector: 'node[group=\"L0\"]',\n";
-		result += "\t\t\tstyle: {\n";
-		result += "\t\t\t\tshape: 'roundrectangle',\n";
-		result += "\t\t\t\t'background-color': 'red',\n";
-		result += "\t\t\t\t'background-image': 'attach/KM_TOP/b72ba78e278e5f14.JPG',\n";
-		result += "\t\t\t\twidth: 50,\n";
-		result += "\t\t\t\theight: 50,\n";
-		result += "\t\t\t\topacity:0.9,\n";
-		result += "\t\t\t\t'font-size': 30,\n";
-		result += "\t\t\t\tlabel: 'data(id)'\n";
-		result += "\t\t\t}\n";
-		result += "\t\t},\n";
-		result += "\t\t{\n";
-		result += "\t\t\tselector: 'node[group=\"L1\"]',\n";
-		result += "\t\t\tstyle: {\n";
-		result += "\t\t\t\tshape: 'roundrectangle',\n";
-		result += "\t\t\t\t'background-color': 'blue',\n";
-		result += "\t\t\t\twidth: 40,\n";
-		result += "\t\t\t\theight: 40,\n";
-		result += "\t\t\t\topacity:0.8,\n";
-		result += "\t\t\t\t'font-size': 20,\n";
-		result += "\t\t\t\tlabel: 'data(id)'\n";
-		result += "\t\t\t}\n";
-		result += "\t\t},\n";
-		result += "\t\t{\n";
-		result += "\t\t\tselector: 'node[group=\"L2\"]',\n";
-		result += "\t\t\tstyle: {\n";
-		result += "\t\t\t\tshape: 'roundrectangle',\n";
-		result += "\t\t\t\t'background-color': 'blue',\n";
-		result += "\t\t\t\twidth: 30,\n";
-		result += "\t\t\t\theight: 30,\n";
-		result += "\t\t\t\topacity:0.7,\n";
-		result += "\t\t\t\t'font-size': 10,\n";
-		result += "\t\t\t\tlabel: 'data(id)'\n";
-		result += "\t\t\t}\n";
-		result += "\t\t},\n";
-		result += "\t\t{\n";
-		result += "\t\t\tselector: 'edge',\n";
-		result += "\t\t\tstyle: {\n";
-		result += "\t\t\t\t'width':1,\n";
-		result += "\t\t\t\t'line-color': '#c0c',\n";
-		result += "\t\t\t\t'target-arrow-shape': 'triangle',\n";
-		result += "\t\t\t\t'target-arrow-color': '#c0c',\n";
-		result += "\t\t\t\t'curve-style': 'segments'\n";
-		result += "\t\t\t}\n";
-		result += "\t\t}]\n";
-		result += "\t});\n";
-		result += "\tcy.on('click', 'node', function(evt) {\n";
-		result += "\t\tvar arg = new Object;\n";
-		result += "\t\tvar pair=location.search.substring(1).split('&');\n";
-		result += "\t\tfor(var i=0;pair[i];i++) {\n";
-		result += "\t\t\tvar kv = pair[i].split('=');\n";
-		result += "\t\t\targ[kv[0]]=kv[1];\n";
-		result += "\t\t}\n";
-		result += "\t\twindow.location.href='Wiki.jsp?page='+arg.page+'&target_node='+this.id();\n";
-		result += "\t});\n";
-		result += "</script>\n";
 
 		return result;
 	}
@@ -208,10 +260,12 @@ public class CytoscapePlugin implements WikiPlugin {
 	 * @param edgeSet
 	 * @param depth
 	 * @return
+	 * @throws ProviderException
 	 */
 	protected String readNodeAndEdge(WikiEngine engine, String pagename, TreeSet<PageInformation> nodeSet,
-			TreeSet<Link> edgeSet, int depth, int max_depth) {
+			TreeSet<Link> edgeSet, int depth, int max_depth) throws ProviderException {
 		String result = "";
+		AttachmentManager attachmentManager = engine.getAttachmentManager();
 		PageInformation tmpPageInfo = new PageInformation(pagename, max_depth - depth);
 		if (depth > 0) {
 			checkAndAdd(nodeSet, tmpPageInfo);
@@ -219,6 +273,22 @@ public class CytoscapePlugin implements WikiPlugin {
 			if (page != null) {
 				String pagedata = engine.getPureText(page);
 				WikiContext targetContext = new WikiContext(engine, page);
+
+				@SuppressWarnings("rawtypes")
+				Collection list = attachmentManager.listAttachments(page);
+				if (list.size() > 0) {
+					Object tempObj = list.iterator().next();
+					if (tempObj instanceof Attachment) {
+						String fileName = ((Attachment) tempObj).getFileName();
+						if (Picture.isPictureFileName(fileName)) {
+							try {
+								tmpPageInfo.setPicture(new Picture(fileName));
+							} catch (IllegalFileNameException e) {
+								// do nothing
+							}
+						}
+					}
+				}
 
 				LinkCollector localCollector = new LinkCollector();
 				LinkCollector extCollector = new LinkCollector();
@@ -235,6 +305,7 @@ public class CytoscapePlugin implements WikiPlugin {
 			} else {
 				result += pagename + " seem not exist.<br>\n";
 			}
+			// checkAndAdd(nodeSet, tmpPageInfo);
 		}
 		return result;
 	}
