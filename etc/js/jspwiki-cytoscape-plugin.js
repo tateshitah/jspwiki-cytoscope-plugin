5720e33a59f3d9f270b990fd6edbfeb279e19dff
Hiroaki Tateshita <tateshita.hiroaki@gmail.com>
2018-07-08 15:51:17 +0900

    little update

--- etc/js/jspwiki-cytoscape-plugin.js
+++ etc/js/jspwiki-cytoscape-plugin.js
@@ -1,108 +1,135 @@
+/*
+
+Copyright (c) 2018 Hiroaki Tateshita
+
+Permission is hereby granted, free of charge, to any person obtaining a copy 
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+copies of the Software, and to permit persons to whom the Software is furnished
+to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all 
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ */
+
+/*
+ * version 1.1.1a
+ */
+
 const layout_param_cose = {
-		name : 'cose',
-		idealEdgeLength : 100,
-		nodeOverlap : 20,
-		refresh : 20,
-		fit : true,
-		padding : 30,
-		randomize : false,
-		componentSpacing : 100,
-		nodeRepulsion : 400000,
-		edgeElasticity : 100,
-		nestingFactor : 5,
-		gravity : 80,
-		numIter : 1000,
-		initialTemp : 200,
-		coolingFactor : 0.95,
-		minTemp : 1.0
+	name : 'cose',
+	idealEdgeLength : 100,
+	nodeOverlap : 20,
+	refresh : 20,
+	fit : true,
+	padding : 30,
+	randomize : false,
+	componentSpacing : 100,
+	nodeRepulsion : 400000,
+	edgeElasticity : 100,
+	nestingFactor : 5,
+	gravity : 80,
+	numIter : 1000,
+	initialTemp : 200,
+	coolingFactor : 0.95,
+	minTemp : 1.0
 };
 const layout_param_cola = {
 	name : 'cola',
-	maxSimulationTime: 600000,
-	padding: 10
+	maxSimulationTime : 600000,
+	padding : 10
 };
+let depth;
 let cy = cytoscape({
 	container : document.getElementById('cy'),
 });
 cy.style([ {
-		selector : 'node',
-		style : {
-			shape : 'roundrectangle',
-			width : 20,
-			height : 20,
-			opacity : 0.3,
-			'background-color' : 'blue',
-		}
-	}, {
-		selector : 'node[group="L0"]',
-		style : {
-			shape : 'roundrectangle',
-			'background-color' : 'blue',
-			'background-image' : 'data(pic)',
-			'background-fit' : 'contain',
-			width : 100,
-			height : 100,
-			opacity : 0.8,
-			'font-size' : 30,
-			label : 'data(id)'
-		}
-	}, {
-		selector : 'node[group="L0WOP"]',
-		style : {
-			shape : 'roundrectangle',
-			'background-color' : 'blue',
-			width : 100,
-			height : 100,
-			opacity : 0.8,
-			'font-size' : 30,
-			label : 'data(id)'
-		}
-	}, {
-		selector : 'node[group="L1"]',
-		style : {
-			shape : 'roundrectangle',
-			'background-color' : 'blue',
-			'background-image' : 'data(pic)',
-			'background-fit' : 'contain',
-			width : 60,
-			height : 60,
-			opacity : 0.7,
-			'font-size' : 20,
-			label : 'data(id)'
-		}
-	}, {
-		selector : 'node[group="L1WOP"]',
-		style : {
-			shape : 'roundrectangle',
-			'background-color' : 'blue',
-			width : 60,
-			height : 60,
-			opacity : 0.7,
-			'font-size' : 20,
-			label : 'data(id)'
-		}
-	}, {
-		selector : 'node[group="L2"]',
-		style : {
-			shape : 'roundrectangle',
-			'background-color' : 'blue',
-			width : 40,
-			height : 40,
-			opacity : 0.6,
-			'font-size' : 10,
-			label : 'data(id)'
-		}
-	}, {
-		selector : 'edge',
-		style : {
-			'width' : 1,
-			'line-color' : '#c0c',
-			'target-arrow-shape' : 'triangle',
-			'target-arrow-color' : '#c0c',
-			'curve-style' : 'segments'
-		}
-	} ]
-);
+	selector : 'node',
+	style : {
+		shape : 'roundrectangle',
+		width : 20,
+		height : 20,
+		opacity : 0.3,
+		'background-color' : 'blue',
+	}
+}, {
+	selector : 'node[group="L0"]',
+	style : {
+		shape : 'roundrectangle',
+		'background-color' : 'blue',
+		'background-image' : 'data(pic)',
+		'background-fit' : 'contain',
+		width : 100,
+		height : 100,
+		opacity : 0.8,
+		'font-size' : 30,
+		label : 'data(id)'
+	}
+}, {
+	selector : 'node[group="L0WOP"]',
+	style : {
+		shape : 'roundrectangle',
+		'background-color' : 'blue',
+		width : 100,
+		height : 100,
+		opacity : 0.8,
+		'font-size' : 30,
+		label : 'data(id)'
+	}
+}, {
+	selector : 'node[group="L1"]',
+	style : {
+		shape : 'roundrectangle',
+		'background-color' : 'blue',
+		'background-image' : 'data(pic)',
+		'background-fit' : 'contain',
+		width : 60,
+		height : 60,
+		opacity : 0.7,
+		'font-size' : 20,
+		label : 'data(id)'
+	}
+}, {
+	selector : 'node[group="L1WOP"]',
+	style : {
+		shape : 'roundrectangle',
+		'background-color' : 'blue',
+		width : 60,
+		height : 60,
+		opacity : 0.7,
+		'font-size' : 20,
+		label : 'data(id)'
+	}
+}, {
+	selector : 'node[group="L2"]',
+	style : {
+		shape : 'roundrectangle',
+		'background-color' : 'blue',
+		width : 40,
+		height : 40,
+		opacity : 0.6,
+		'font-size' : 10,
+		label : 'data(id)'
+	}
+}, {
+	selector : 'edge',
+	style : {
+		'width' : 1,
+		'line-color' : '#c0c',
+		'target-arrow-shape' : 'triangle',
+		'target-arrow-color' : '#c0c',
+		'curve-style' : 'segments'
+	}
+} ]);
 cy.on('tap', 'node', function(evt) {
 	data_array = [];
 	for (let i = 0; i < node_array.length; i++) {
@@ -111,29 +138,37 @@ cy.on('tap', 'node', function(evt) {
 	cy.remove('node');
 	cy
 			.add(readNodeAndEdge(data_array, node_array, edge_array, this.id(),
-					2, 2));
-	var layout = cy.layout(layout_param);
+					depth, depth));
+	let layout = cy.layout(layout_param);
 	layout.run();
 });
 cy.on('cxttap', 'node', function(evt) {
 	window.location.href = 'Wiki.jsp?page=' + this.id();
 });
+/**
+ * 
+ * @param data_array
+ *            let variable for cytoscape view
+ * @param node_array
+ *            let variable for all nodes of wiki
+ * @param edge_array
+ *            const variable for all edges of wiki
+ * @param target
+ *            target node
+ * @param depth
+ * @param depth_origin
+ * @returns
+ */
 function readNodeAndEdge(data_array, node_array, edge_array, target, depth,
 		depth_origin) {
 	for (let i = 0; i < node_array.length; i++) {
 		if (node_array[i].data.id == target) {
-			if (node_array[i].data['group'] == null) {
-				if (depth_origin - depth < 2) {
-					if (node_array[i].data.pic == null) {
-						node_array[i].data['group'] = 'L'
-								+ (depth_origin - depth) + 'WOP';
-					} else {
-						node_array[i].data['group'] = 'L'
-								+ (depth_origin - depth) + 'WOP';
-					}
-				} else {
-					node_array[i].data['group'] = 'L' + (depth_origin - depth);
-				}
+			// group should be updated when
+			// group is not defined or
+			// group is defined but more closer node was found
+			if (node_array[i].data['group'] == null
+					|| node_array[i].data['steps'] > depth_origin - depth) {
+				addGroupAndSteps(node_array[i].data, depth_origin - depth);
 			}
 			data_array.push({
 				group : 'nodes',
@@ -156,4 +191,18 @@ function readNodeAndEdge(data_array, node_array, edge_array, target, depth,
 		}
 	}
 	return data_array;
+}
+function addGroupAndSteps(data, steps) {
+	if (steps < 2) {
+		if (data.pic == null) {
+			data['group'] = 'L' + steps + 'WOP';
+			data['steps'] = steps;
+		} else {
+			data['group'] = 'L' + steps+'WOP';
+			data['steps'] = steps;
+		}
+	} else {
+		data['group'] = 'L' + steps;
+		data['steps'] = steps;
+	}
 }
--- src/main/java/org/braincopy/jspwiki/plugin/CytoscapePlugin.java
+++ src/main/java/org/braincopy/jspwiki/plugin/CytoscapePlugin.java
@@ -139,112 +139,85 @@ public class CytoscapePlugin implements WikiPlugin {
 			result += "var data_array = [];\n";
 			result += getNodeDataJson(nodeSet);
 			result += getEdgeDataJson(edgeSet);
-			result += getLayoutParam(layout);
-			result += "\tvar cy = cytoscape({\n";
-			result += "\t\tcontainer: document.getElementById('cy'),\n";
-			result += "\t\telements: readNodeAndEdge(data_array, node_array, edge_array, '" + pagename + "', " + depth
-					+ ", " + depth + "),\n";
+			result += "</script>\r\n";
 
-			result += "\t\tlayout : layout_param,\n";
-			result += "\t\tstyle: [{\n";
-			result += "\t\t\tselector: 'node',\n";
-			result += "\t\t\tstyle: {\n";
-			result += "\t\t\t\tshape: 'roundrectangle',\n";
-			result += "\t\t\t\twidth: 20,\n";
-			result += "\t\t\t\theight: 20,\n";
-			result += "\t\t\t\topacity:0.3,\n";
-			result += "\t\t\t\t'background-color': 'blue',\n";
-			result += "\t\t\t}\n";
-			result += "\t\t},\n";
-			result += "\t\t{\n";
-			result += "\t\t\tselector: 'node[group=\"L0\"]',\n";
-			result += "\t\t\tstyle: {\n";
-			result += "\t\t\t\tshape: 'roundrectangle',\n";
-			result += "\t\t\t\t'background-color': 'blue',\n";
-			result += "\t\t\t\t'background-image': 'data(pic)',\n";
-			result += "\t\t\t\t'background-fit': 'contain',\n";
-			result += "\t\t\t\twidth: 100,\n";
-			result += "\t\t\t\theight: 100,\n";
-			result += "\t\t\t\topacity:0.8,\n";
-			result += "\t\t\t\t'font-size': 30,\n";
-			result += "\t\t\t\tlabel: 'data(id)'\n";
-			result += "\t\t\t}\n";
-			result += "\t\t},\n";
-			result += "\t\t{\n";
-			result += "\t\t\tselector: 'node[group=\"L0WOP\"]',\n";
-			result += "\t\t\tstyle: {\n";
-			result += "\t\t\t\tshape: 'roundrectangle',\n";
-			result += "\t\t\t\t'background-color': 'blue',\n";
-			result += "\t\t\t\twidth: 100,\n";
-			result += "\t\t\t\theight: 100,\n";
-			result += "\t\t\t\topacity:0.8,\n";
-			result += "\t\t\t\t'font-size': 30,\n";
-			result += "\t\t\t\tlabel: 'data(id)'\n";
-			result += "\t\t\t}\n";
-			result += "\t\t},\n";
-			result += "\t\t{\n";
-			result += "\t\t\tselector: 'node[group=\"L1\"]',\n";
-			result += "\t\t\tstyle: {\n";
-			result += "\t\t\t\tshape: 'roundrectangle',\n";
-			result += "\t\t\t\t'background-color': 'blue',\n";
-			result += "\t\t\t\t'background-image': 'data(pic)',\n";
-			result += "\t\t\t\t'background-fit': 'contain',\n";
-			result += "\t\t\t\twidth: 60,\n";
-			result += "\t\t\t\theight: 60,\n";
-			result += "\t\t\t\topacity:0.7,\n";
-			result += "\t\t\t\t'font-size': 20,\n";
-			result += "\t\t\t\tlabel: 'data(id)'\n";
-			result += "\t\t\t}\n";
-			result += "\t\t},\n";
-			result += "\t\t{\n";
-			result += "\t\t\tselector: 'node[group=\"L1WOP\"]',\n";
-			result += "\t\t\tstyle: {\n";
-			result += "\t\t\t\tshape: 'roundrectangle',\n";
-			result += "\t\t\t\t'background-color': 'blue',\n";
-			result += "\t\t\t\twidth: 60,\n";
-			result += "\t\t\t\theight: 60,\n";
-			result += "\t\t\t\topacity:0.7,\n";
-			result += "\t\t\t\t'font-size': 20,\n";
-			result += "\t\t\t\tlabel: 'data(id)'\n";
-			result += "\t\t\t}\n";
-			result += "\t\t},\n";
-			result += "\t\t{\n";
-			result += "\t\t\tselector: 'node[group=\"L2\"]',\n";
-			result += "\t\t\tstyle: {\n";
-			result += "\t\t\t\tshape: 'roundrectangle',\n";
-			result += "\t\t\t\t'background-color': 'blue',\n";
-			result += "\t\t\t\twidth: 40,\n";
-			result += "\t\t\t\theight: 40,\n";
-			result += "\t\t\t\topacity:0.6,\n";
-			result += "\t\t\t\t'font-size': 10,\n";
-			result += "\t\t\t\tlabel: 'data(id)'\n";
-			result += "\t\t\t}\n";
-			result += "\t\t},\n";
-			result += "\t\t{\n";
-			result += "\t\t\tselector: 'edge',\n";
-			result += "\t\t\tstyle: {\n";
-			result += "\t\t\t\t'width':1,\n";
-			result += "\t\t\t\t'line-color': '#c0c',\n";
-			result += "\t\t\t\t'target-arrow-shape': 'triangle',\n";
-			result += "\t\t\t\t'target-arrow-color': '#c0c',\n";
-			result += "\t\t\t\t'curve-style': 'segments'\n";
-			result += "\t\t\t}\n";
-			result += "\t\t}]\n";
-			result += "\t});\n";
-			result += "\tcy.on('tap', 'node', function(evt) {\n";
-			result += "\t\tdata_array = [];\r\n";
-			result += "\t\tfor (let i = 0; i < node_array.length; i++){\n";
-			result += "\t\t\tnode_array[i].data['group'] = null;\n\t\t}\n";
-			result += "\t\tcy.remove('node');\n";
-			result += "\t\tcy.add(readNodeAndEdge(data_array, node_array, edge_array, this.id(), " + depth + ", "
-					+ depth + "));\n";
-			result += "\t\tvar layout = cy.layout(layout_param);\n";
-			result += "\t\tlayout.run();\n";
-			result += "\t});\n";
-			result += "\tcy.on('cxttap', 'node', function(evt) {\n";
-			result += "\t\twindow.location.href='Wiki.jsp?page='+this.id();\n";
-			result += "\t});\n";
-			result += getRecursiveFunction();
+			// result += getLayoutParam(layout);
+			result += "<script src='https://braincopy.org/WebContent/js/jspwiki-cytoscape-plugin.js'></script>\r\n";
+			result += "<script type=\"text/javascript\">\r\n";
+			result += "\tdepth = " + depth + ";\r\n";
+			result += "\tcy.add(readNodeAndEdge(data_array, node_array, edge_array, 'Main', depth, depth));\r\n";
+			if (layout.equals("cose")) {
+				result += "\tlet layout_param = layout_param_cose;\r\n";
+			} else {
+				result += "\tlet layout_param = layout_param_cola;\r\n";
+			}
+			result += "\tconst layout = cy.layout(layout_param);\r\n";
+			result += "\tlayout.run();\r\n";
+			/*
+			 * result += "\tvar cy = cytoscape({\n"; result +=
+			 * "\t\tcontainer: document.getElementById('cy'),\n"; result +=
+			 * "\t\telements: readNodeAndEdge(data_array, node_array, edge_array, '" +
+			 * pagename + "', " + depth + ", " + depth + "),\n";
+			 * 
+			 * result += "\t\tlayout : layout_param,\n"; result += "\t\tstyle: [{\n"; result
+			 * += "\t\t\tselector: 'node',\n"; result += "\t\t\tstyle: {\n"; result +=
+			 * "\t\t\t\tshape: 'roundrectangle',\n"; result += "\t\t\t\twidth: 20,\n";
+			 * result += "\t\t\t\theight: 20,\n"; result += "\t\t\t\topacity:0.3,\n"; result
+			 * += "\t\t\t\t'background-color': 'blue',\n"; result += "\t\t\t}\n"; result +=
+			 * "\t\t},\n"; result += "\t\t{\n"; result +=
+			 * "\t\t\tselector: 'node[group=\"L0\"]',\n"; result += "\t\t\tstyle: {\n";
+			 * result += "\t\t\t\tshape: 'roundrectangle',\n"; result +=
+			 * "\t\t\t\t'background-color': 'blue',\n"; result +=
+			 * "\t\t\t\t'background-image': 'data(pic)',\n"; result +=
+			 * "\t\t\t\t'background-fit': 'contain',\n"; result += "\t\t\t\twidth: 100,\n";
+			 * result += "\t\t\t\theight: 100,\n"; result += "\t\t\t\topacity:0.8,\n";
+			 * result += "\t\t\t\t'font-size': 30,\n"; result +=
+			 * "\t\t\t\tlabel: 'data(id)'\n"; result += "\t\t\t}\n"; result += "\t\t},\n";
+			 * result += "\t\t{\n"; result += "\t\t\tselector: 'node[group=\"L0WOP\"]',\n";
+			 * result += "\t\t\tstyle: {\n"; result += "\t\t\t\tshape: 'roundrectangle',\n";
+			 * result += "\t\t\t\t'background-color': 'blue',\n"; result +=
+			 * "\t\t\t\twidth: 100,\n"; result += "\t\t\t\theight: 100,\n"; result +=
+			 * "\t\t\t\topacity:0.8,\n"; result += "\t\t\t\t'font-size': 30,\n"; result +=
+			 * "\t\t\t\tlabel: 'data(id)'\n"; result += "\t\t\t}\n"; result += "\t\t},\n";
+			 * result += "\t\t{\n"; result += "\t\t\tselector: 'node[group=\"L1\"]',\n";
+			 * result += "\t\t\tstyle: {\n"; result += "\t\t\t\tshape: 'roundrectangle',\n";
+			 * result += "\t\t\t\t'background-color': 'blue',\n"; result +=
+			 * "\t\t\t\t'background-image': 'data(pic)',\n"; result +=
+			 * "\t\t\t\t'background-fit': 'contain',\n"; result += "\t\t\t\twidth: 60,\n";
+			 * result += "\t\t\t\theight: 60,\n"; result += "\t\t\t\topacity:0.7,\n"; result
+			 * += "\t\t\t\t'font-size': 20,\n"; result += "\t\t\t\tlabel: 'data(id)'\n";
+			 * result += "\t\t\t}\n"; result += "\t\t},\n"; result += "\t\t{\n"; result +=
+			 * "\t\t\tselector: 'node[group=\"L1WOP\"]',\n"; result += "\t\t\tstyle: {\n";
+			 * result += "\t\t\t\tshape: 'roundrectangle',\n"; result +=
+			 * "\t\t\t\t'background-color': 'blue',\n"; result += "\t\t\t\twidth: 60,\n";
+			 * result += "\t\t\t\theight: 60,\n"; result += "\t\t\t\topacity:0.7,\n"; result
+			 * += "\t\t\t\t'font-size': 20,\n"; result += "\t\t\t\tlabel: 'data(id)'\n";
+			 * result += "\t\t\t}\n"; result += "\t\t},\n"; result += "\t\t{\n"; result +=
+			 * "\t\t\tselector: 'node[group=\"L2\"]',\n"; result += "\t\t\tstyle: {\n";
+			 * result += "\t\t\t\tshape: 'roundrectangle',\n"; result +=
+			 * "\t\t\t\t'background-color': 'blue',\n"; result += "\t\t\t\twidth: 40,\n";
+			 * result += "\t\t\t\theight: 40,\n"; result += "\t\t\t\topacity:0.6,\n"; result
+			 * += "\t\t\t\t'font-size': 10,\n"; result += "\t\t\t\tlabel: 'data(id)'\n";
+			 * result += "\t\t\t}\n"; result += "\t\t},\n"; result += "\t\t{\n"; result +=
+			 * "\t\t\tselector: 'edge',\n"; result += "\t\t\tstyle: {\n"; result +=
+			 * "\t\t\t\t'width':1,\n"; result += "\t\t\t\t'line-color': '#c0c',\n"; result
+			 * += "\t\t\t\t'target-arrow-shape': 'triangle',\n"; result +=
+			 * "\t\t\t\t'target-arrow-color': '#c0c',\n"; result +=
+			 * "\t\t\t\t'curve-style': 'segments'\n"; result += "\t\t\t}\n"; result +=
+			 * "\t\t}]\n"; result += "\t});\n"; result +=
+			 * "\tcy.on('tap', 'node', function(evt) {\n"; result +=
+			 * "\t\tdata_array = [];\r\n"; result +=
+			 * "\t\tfor (let i = 0; i < node_array.length; i++){\n"; result +=
+			 * "\t\t\tnode_array[i].data['group'] = null;\n\t\t}\n"; result +=
+			 * "\t\tcy.remove('node');\n"; result +=
+			 * "\t\tcy.add(readNodeAndEdge(data_array, node_array, edge_array, this.id(), "
+			 * + depth + ", " + depth + "));\n"; result +=
+			 * "\t\tvar layout = cy.layout(layout_param);\n"; result +=
+			 * "\t\tlayout.run();\n"; result += "\t});\n"; result +=
+			 * "\tcy.on('cxttap', 'node', function(evt) {\n"; result +=
+			 * "\t\twindow.location.href='Wiki.jsp?page='+this.id();\n"; result +=
+			 * "\t});\n"; result += getRecursiveFunction();
+			 */
 			result += "</script>\n";
 
 		} catch (ProviderException e) {
